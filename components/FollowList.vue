abe2c95213a54bc17af32c6edce583e947c44e92
Gwon Gyeongmin <kwon951214@naver.com>
2020-01-03 23:31:43 +0900

    vue7

---
+++ components/FollowList.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-list-item-tile>
+        <ul>
+            <li>
+                <span>GWON</span>
+                <v-icon>mdi-minus-circle-outline</v-icon>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <span>GGM</span>
+                <v-icon>mdi-minus-circle-outline</v-icon>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <span>it's me</span>
+                <v-icon>mdi-minus-circle-outline</v-icon>
+            </li>
+        </ul>
+    </v-list-item-tile>
+</template>
+<script>
+    export default {}
+</script>
+<style></style>

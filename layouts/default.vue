b2c94246d2721697cdf8680936a2248382a95a3b
Gwon Gyeongmin <kwon951214@naver.com>
2020-01-04 22:08:53 +0900

    vue2-1

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,10 +5,10 @@
                 <v-toolbar-title>
                     <nuxt-link to="/">Nodebird</nuxt-link>
                 </v-toolbar-title>
-                <v-spacer></v-spacer>
+                <v-spacer/>
                 <v-toolbar-items>
                     <v-text-field label="검색" hide-details prepend-icon="mdi-magnify"
-                                  :style="{ display: 'flex', alignItems: 'center' }" />
+                                  :style="{ display: 'flex', alignItems: 'center' }"/>
                     <v-btn text nuxt to="/profile">
                         <div>프로필</div>
                     </v-btn>
@@ -18,24 +18,37 @@
                 </v-toolbar-items>
             </v-toolbar>
         </nav>
+        <div>{{name}}</div>
+        <v-btn @click="onChangeName">바이바이</v-btn>
         <v-row no-gutters>
             <v-col cols="12" xs="12" md="4">
                 <LoginForm></LoginForm>
             </v-col>
             <v-col cols="12" xs="12" md="8">
 
-                <nuxt />
+                <nuxt/>
             </v-col>
         </v-row>
     </v-app>
 </template>
 <script>
     import LoginForm from '~/components/LoginForm';
-export default {
-components:{
-    LoginForm
-}
-};
+
+    export default {
+        components: {
+            LoginForm
+        },
+        compputed: {
+            name() {
+                return this.$store.state.posts.name;
+            }
+        },
+        methods: {
+            onChangeName() {
+                this.$store.commit('posts/BYE');
+            }
+        }
+    };
 </script>
 <style>
     .v-application a {

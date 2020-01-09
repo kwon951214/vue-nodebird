3334224dfab4da55bc3ad2db9e6935ca7175ae6d
Gwon Gyeongmin <kwon951214@naver.com>
2020-01-09 23:09:50 +0900

    vue2-6

---
+++ components/commentForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-form ref="form" v-model="valid" style="position: relative" @submit.prevent="onsubmitForm">
+        <v-textarea
+            v-model="content"
+            filled
+            label="댓글 달기"
+            :hide-details="hideDetails"
+            :success="success"
+            :success-messages="successMessages"
+            @input="onChangeTextarea"
+            />
+        <v-btn color="green" dark absolute top right type="submit">삐약</v-btn>
+    </v-form>
+</template>
+
+<script>
+
+</script>
+<style>
+
+</style>

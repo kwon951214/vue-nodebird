d92f2db53d2f5f6a0c75b38c67dd95a8a86f0ecd
Gwon Gyeongmin <kwon951214@naver.com>
2020-01-19 23:45:42 +0900

    vue3-7

--- pages/profile.vue
+++ pages/profile.vue
@@ -6,7 +6,7 @@
                     <v-subheader>내 프로필</v-subheader>
                     <v-form v-model="valid" @submit.prevent="onChangeNickname">
                         <v-text-field v-model="nickname" label="닉네임" :rules="nicknameRules" required/>
-                        <v-btn color="blue" type="submit">수정</v-btn>
+                        <v-btn dark color="blue" type="submit">수정</v-btn>
                     </v-form>
                 </v-container>
             </v-card>
@@ -14,14 +14,14 @@
                 <v-container>
                     <v-subheader>팔로잉</v-subheader>
                     <follow-list :users="followingList" :remove="removeFollowing"/>
-                    <v-btn dark color="blue" style="width: 100%">더보기</v-btn>
+                    <v-btn @click="loadMoreFollowings" v-if="hasMoreFollowing" dark color="blue" style="width: 100%">더보기</v-btn>
                 </v-container>
             </v-card>
             <v-card style="margin-bottom: 20px">
                 <v-container>
                     <v-subheader>팔로워</v-subheader>
                     <follow-list :users="followerList" :remove="removeFollower"/>
-                    <v-btn dark color="blue" style="width: 100%">더보기</v-btn>
+                    <v-btn @click="loadMoreFollowers" v-if="hasMoreFollower" dark color="blue" style="width: 100%">더보기</v-btn>
                 </v-container>
             </v-card>
         </v-container>
@@ -52,6 +52,16 @@
         followingList() {
             return this.$store.state.users.followingList;
         },
+        hasMoreFollowing(){
+          return this.$store.state.users.hasMoreFollowing;
+        },
+        hasMoreFollower(){
+            return this.$store.state.users.hasMoreFollower;
+        },
+        fetch({ store }){
+            store.dispatch('users/loadFollowers');
+            store.dispatch('users/loadFollowings');
+        },
         methods: {
             onChangeNickname() {
                 this.$store.dispatch('users/changeNickname', {
@@ -65,6 +75,12 @@
             },
             removeFollower(id) {
                 this.$store.dispatch('users/removeFollowing', {id});
+            },
+            loadMoreFollowers(){
+                this.$store.dispatch('users/loadFollowers');
+            },
+            loadMoreFollowings(){
+                this.$store.dispatch('users/loadFollowings');
             }
         },
         head() {
--- store/users.js
+++ store/users.js
@@ -1,20 +1,15 @@
 export const state = () => ({
     me: null,
-    followerList: [{
-        id: 1,
-        nickname: '나야나'
-    }, {
-        id: 2,
-        nickname: 'Gwon'
-    },{
-        id: 3,
-        nickname: 'GM'
-    }],
-    followingList: [{
+    followerList: [],
+    followingList: [],
+    hasMoreFollower: true,
+    hasMoreFollowing: true
 
-    }]
 });
 
+const totalFollowers = 8;
+const totalFollowings = 6;
+const limit = 3;
 export const mutations = {
     setMe(state, payload) {
         state.me = payload;
@@ -36,6 +31,24 @@ export const mutations = {
         const index = state.followerList.findIndex(v => v.id === payload.id);
         state.followerList.splice(index, 1);
     },
+    loadFollowings(state) {
+        const diff = totalFollowings - state.followingList.length;
+        const fakeUsers = Array(diff > limit ? limit : diff).fill().map(v => ({
+            id: Math.random().toString(),
+            nickname: Math.floor(Math.random() * 1000)
+        }));
+        state.followingList = state.followingList.concat(fakeUsers);
+        state.hasMoreFollowing = fakeUsers.length === limit;
+    },
+    loadFollowers(state) {
+        const diff = totalFollowers - state.followerList.length;
+        const fakeUsers = Array(diff > limit ? limit : diff).fill().map(v => ({
+            id: Math.random().toString(),
+            nickname: Math.floor(Math.random() * 1000)
+        }));
+        state.followerList = state.followerList.concat(fakeUsers);
+        state.hasMoreFollower = fakeUsers.length === limit;
+    }
 };
 
 export const actions = {
@@ -63,5 +76,16 @@ export const actions = {
     },
     removeFollower() {
         commit('removeFollower', payload);
+    },
+    loadFollowers({commit, state}, payload) {
+        if (state.hasMoreFollower) {
+
+            commit('loadFollowers');
+        }
+    },
+    loadFollowings({commit}, payload) {
+        if (state.hasMoreFollowing) {
+            commit('loadFollowings');
+        }
     }
 };
